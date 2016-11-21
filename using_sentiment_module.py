96cb383569377f7e564e274142d612d41ca83a1f
SanchitGupta001 <sanchit.gupta011@gmail.com>
2016-11-22 03:20:54 +0530

    Added two scripts sentiment_mod and using_sentiment_mod

---
+++ using_sentiment_module.py
@@ -0,0 +1,5 @@
+import sentiment_mod as s
+
+
+print(s.sentiment("This movie was rubbish! The acting was bad, plot was non-sense, and there were no pythons..."))
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons..."))
